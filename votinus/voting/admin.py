94dbfa1502b7d6d59af4ae9ebf395dae688021aa
Daniel García Moreno <danigm@wadobo.com>
2018-02-23 12:47:44 +0100

    voting: Modelo de votación y formluario de admin
    Modelo básico de votación con una sóla pregunta. Los números de las
    opciones se autogeneran a partir del 2, porque el 1 no se puede cifrar
    bien con el algoritmo de cifrado.
    
    see #10

--- votinus/voting/admin.py
+++ votinus/voting/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import QuestionOption
+from .models import Question
+from .models import Voting
+
+
+class QuestionOptionInline(admin.TabularInline):
+    model = QuestionOption
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [QuestionOptionInline]
+
+
+class VotingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
+    readonly_fields = ('start_date', 'end_date', 'pub_key')
+
+
+admin.site.register(Voting, VotingAdmin)
+admin.site.register(Question, QuestionAdmin)
--- votinus/voting/models.py
+++ votinus/voting/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from mixnet.models import Auth, Key
+
+
+class Question(models.Model):
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.desc
+
+
+class QuestionOption(models.Model):
+    question = models.ForeignKey(Question, related_name='options', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField(blank=True, null=True)
+    option = models.TextField()
+
+    def save(self):
+        if not self.number:
+            self.number = self.question.options.count() + 2
+        return super().save()
+
+    def __str__(self):
+        return '{} ({}): {}'.format(self.question.desc, self.number, self.option)
+
+
+class Voting(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True, null=True)
+    question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE)
+
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    pub_key = models.OneToOneField(Key, related_name='voting', blank=True, null=True, on_delete=models.SET_NULL)
+    auths = models.ManyToManyField(Auth, related_name='votings')
+
+    def __str__(self):
+        return self.name
