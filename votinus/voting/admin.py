085aa3fb508deb01cd9f09edbf1573d68ae0104f
Daniel García Moreno <danigm@wadobo.com>
2018-02-23 13:29:11 +0100

    voting: Acción para comenzar/terminar una votación
    He añadido dos acciones una para comenzar la votación, que creará una
    mixnet para conseguir la clave pública de cifrado y otra para terminar
    la votación.
    
    En la acción de terminar la votación se puede añadir el mezclado y
    descifrado de votos cuando tengamos implementado el almacenamiento.
    
    see #10

--- votinus/voting/admin.py
+++ votinus/voting/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from .models import QuestionOption
 from .models import Question
 from .models import Voting
+
+
+def start(modeladmin, request, queryset):
+    for v in queryset.all():
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+
+
+def stop(ModelAdmin, request, queryset):
+    for v in queryset.all():
+        v.end_date = timezone.now()
+        v.save()
 
 
 class QuestionOptionInline(admin.TabularInline):
@@ -16,6 +30,8 @@ class QuestionAdmin(admin.ModelAdmin):
 class VotingAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date')
     readonly_fields = ('start_date', 'end_date', 'pub_key')
+
+    actions = [ start, stop ]
 
 
 admin.site.register(Voting, VotingAdmin)
--- votinus/voting/models.py
+++ votinus/voting/models.py
@@ -1,4 +1,8 @@
+import requests
+
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from mixnet.models import Auth, Key
 
@@ -34,6 +38,23 @@ class Voting(models.Model):
 
     pub_key = models.OneToOneField(Key, related_name='voting', blank=True, null=True, on_delete=models.SET_NULL)
     auths = models.ManyToManyField(Auth, related_name='votings')
+
+    def create_pubkey(self):
+        if self.pub_key or not self.auths.count():
+            return
+
+        auth = self.auths.first()
+        url = "{}/mixnet/".format(auth.url)
+        data = {
+            "voting": self.id,
+            "auths": [ {"name": a.name, "url": a.url} for a in self.auths.all() ],
+        }
+        resp = requests.post(url, json=data)
+        key = resp.json()
+        pk = Key(p=key["p"], g=key["g"], y=key["y"])
+        pk.save()
+        self.pub_key = pk
+        self.save()
 
     def __str__(self):
         return self.name
