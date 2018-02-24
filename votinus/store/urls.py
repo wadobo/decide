d2bcd59622441d77a1d6bc53e80f51768ccddced
Daniel García Moreno <danigm@wadobo.com>
2018-02-24 09:33:17 +0100

    store: vistas de consulta y almacenamiento
    He creado el modelo básico para almacenar votos, relacionando un id de
    votante con una votación y almacenando las dos componentes del voto
    cifrado, a y b.
    
    Todas las consultas se puede hacer utilizando los filtros sobre
    voting_id y voter_id, por lo que para ver si un votante ha votado se
    puede consultar pasando los dos filtros y para obtener el listado de
    votos de una votación se puede filtrar sólo por el voting_id.
    
    See #2

--- votinus/store/urls.py
+++ votinus/store/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.StoreView.as_view(), name='store'),
+]
--- votinus/store/views.py
+++ votinus/store/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import django_filters.rest_framework
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import generics
 
-# Create your views here.
+from .models import Vote
+from .serializers import VoteSerializer
+
+
+class StoreView(generics.ListAPIView):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = ('voting_id', 'voter_id')
+
+    def post(self, request):
+        """
+         * voting: id
+         * voter: id
+         * vote: { "a": int, "b": int }
+        """
+
+        vid = request.data.get('voting')
+        uid = request.data.get('voter')
+        vote = request.data.get('vote')
+
+        if not vid or not uid or not vote:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+        a = vote.get("a")
+        b = vote.get("b")
+
+        # TODO: check permissions with census
+        defs = { "a": a, "b": b }
+        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
+                                          defaults=defs)
+        v.a = a
+        v.b = b
+
+        v.save()
+
+        return  Response({})
