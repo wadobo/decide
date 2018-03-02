28507e15d275cebbbebf994e1344945b25b8079a
Daniel García Moreno <danigm@wadobo.com>
2018-03-02 19:43:10 +0100

    test: Scripts para pruebas de cifrado en js

---
+++ votinus/test-scripts/test-decrypt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+from mixnet.mixcrypt import MixCrypt
+from mixnet.mixcrypt import ElGamal
+
+
+SK = sys.argv[1]
+MSG = sys.argv[2]
+
+p, g, y, x = map(int, SK.split(','))
+a, b = map(int, MSG.split(','))
+
+k = MixCrypt(bits=8)
+k.k = ElGamal.construct((p, g, y, x))
+
+print(k.decrypt((a, b)))
