8141dd98ce5867a7afb34c6b5fdea34c77ace4ac
Daniel García Moreno <danigm@wadobo.com>
2018-03-12 14:39:00 +0100

    booth: Cambina de votación funcional
    Esta es una primera versión de la cabina de votación. Se muestran las
    opciones y según la opción elegida, se cifra en cliente y se envía el
    voto al store.
    
    No se manda el id del votante correctamente porque falta la
    autenticación, pero eso se puede hacer fácilmente pidiendo además del
    voto el usuario y la contraseña para autenticar antes, haciendo una
    petición al submódulo de autenticación.
    
    Close #3

---
+++ votinus/booth/static/crypto/elgamal.js
@@ -0,0 +1,28 @@
+ElGamal = {};
+ElGamal.BITS = 8;
+
+ElGamal.getRandomInteger = function(max) {
+  var bit_length = max.bitLength();
+  var random;
+  random = sjcl.random.randomWords(bit_length, 0);
+  // we get a bit array instead of a BigInteger in this case
+  var rand_bi = new BigInt(sjcl.codec.hex.fromBits(random), 16);
+  return rand_bi.mod(max);
+  return BigInt._from_java_object(random).mod(max);
+};
+
+ElGamal.encrypt = function(pk, m, r) {
+  if (m.equals(BigInt.ZERO))
+    throw "Can't encrypt 0 with El Gamal"
+
+  if (!r) {
+    let q = BigInt.fromInt(2).pow(ElGamal.BITS);
+    let q1 = q.subtract(BigInt.ONE);
+    r = ElGamal.getRandomInteger(q1);
+  }
+
+  var alpha = pk.g.modPow(r, pk.p);
+  var beta = (pk.y.modPow(r, pk.p)).multiply(m).mod(pk.p);
+
+  return { alpha: alpha, beta: beta };
+};
