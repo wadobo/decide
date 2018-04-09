acd652ef8beec72af5e195f20bc2a92ec53fe6c6
Victor Ramirez <virako@wadobo.com>
2018-04-09 12:33:22 +0200

    Added minimal visualizer for voting results

--- votinus/visualizer/urls.py
+++ votinus/visualizer/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from .views import VisualizerView
+
+
+urlpatterns = [
+    path('<int:voting_id>/', VisualizerView.as_view()),
+]
--- votinus/visualizer/views.py
+++ votinus/visualizer/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.http import Http404
 
-# Create your views here.
+from base import mods
+
+
+class VisualizerView(TemplateView):
+    template_name = 'visualizer/visualizer.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            context['voting'] = r[0]
+        except:
+            raise Http404
+
+        return context
