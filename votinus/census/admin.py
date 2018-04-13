bdce33d2b7a5b49b329e4f3bad0635fd6513a68f
Daniel García Moreno <danigm@wadobo.com>
2018-04-13 10:26:01 +0200

    store: check the user token with the uid sent
    In this patch we check if the voter is sending us the real information,
    we check agains the authentication module to get the real user
    information passing the authentication token and if the id isn't the
    same we don't store this vote because is a fraudulent one.

--- votinus/census/admin.py
+++ votinus/census/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import Census
 
 
-admin.site.register(Census)
+class CensusAdmin(admin.ModelAdmin):
+    list_display = ('voting_id', 'voter_id')
+    list_filter = ('voting_id', )
+
+    search_fields = ('voter_id', )
+
+
+admin.site.register(Census, CensusAdmin)
