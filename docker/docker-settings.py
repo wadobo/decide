41c89a9053ee560e473e0f417b81762041012915
Daniel García Moreno <danigm@wadobo.com>
2018-06-06 14:09:21 +0200

    docker: Use the docker port instead devel port

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -27,14 +27,16 @@ MODULES = [
     'voting',
 ]
 
+BASEURL = 'http://localhost:5000'
+
 APIS = {
-    'authentication': 'http://localhost:8000/',
-    'base': 'http://localhost:8000/',
-    'booth': 'http://localhost:8000/',
-    'census': 'http://localhost:8000/',
-    'mixnet': 'http://localhost:8000/',
-    'postproc': 'http://localhost:8000/',
-    'store': 'http://localhost:8000/',
-    'visualizer': 'http://localhost:8000/',
-    'voting': 'http://localhost:8000/',
+    'authentication': 'http://localhost:5000',
+    'base': 'http://localhost:5000',
+    'booth': 'http://localhost:5000',
+    'census': 'http://localhost:5000',
+    'mixnet': 'http://localhost:5000',
+    'postproc': 'http://localhost:5000',
+    'store': 'http://localhost:5000',
+    'visualizer': 'http://localhost:5000',
+    'voting': 'http://localhost:5000',
 }
