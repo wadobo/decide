53011179618efdf973f898bb72aef5f68539cb36
Victor Ramirez <virako@wadobo.com>
2018-12-19 11:53:20 +0100

    Update default database for using postgresql instead sqlite

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -106,8 +106,12 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decide',
+        'USER': 'decide',
+        'PASSWORD': 'decide',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
