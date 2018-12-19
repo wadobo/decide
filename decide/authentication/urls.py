375766c4848c0de4d221f0d20fb549bf77f10fbd
Victor Ramirez <virako@wadobo.com>
2018-12-19 23:49:56 +0100

    Added locust configuration for exec two different estrés test.
    Created register API call for generate fastly several new users

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import GetUserView, LogoutView
+from .views import GetUserView, LogoutView, RegisterView
 
 
 urlpatterns = [
     path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
+    path('register/', RegisterView.as_view()),
 ]
