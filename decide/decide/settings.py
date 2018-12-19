60aa206b948464db8be2de0b5fb7f2e144837223
Victor Ramirez <virako@wadobo.com>
2018-12-19 12:09:27 +0100

    Added coverage support: django-nose and coverage like dependencies and using codacy for report coverage.

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -149,6 +149,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
