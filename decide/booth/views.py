2b873b495672c3218e433255d99a2f7213a9cb5d
Daniel García Moreno <dani@danigm.net>
2019-04-08 11:43:11 +0200

    Fix booth voting with big keys

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -16,6 +16,12 @@ class BoothView(TemplateView):
 
         try:
             r = mods.get('voting', params={'id': vid})
+
+            # Casting numbers to string to manage in javascript with BigInt
+            # and avoid problems with js and big number conversion
+            for k, v in r[0]['pub_key'].items():
+                r[0]['pub_key'][k] = str(v)
+
             context['voting'] = json.dumps(r[0])
         except:
             raise Http404
