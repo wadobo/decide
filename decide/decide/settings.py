c0e6704f68b954126e3ac930f844ff857e1cffb2
Domin-97 <dominmues@gmail.com>
2020-01-22 15:51:25 +0100

    Merge branch 'master' into domin

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -121,9 +121,9 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
+        'NAME': 'ultra',
+        'USER': 'ultra',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -169,13 +169,6 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '465'
-EMAIL_HOST_USER = 'decide123456789@gmail.com'
-EMAIL_HOST_PASSWORD= 'DecideAdmin97'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import VisualizerView
-from . import views
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
-    path('',views.prueba, name='prueba')
 ]
