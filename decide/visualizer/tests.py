3c1712ee015010d2c137d30f6b70e22384b06787
Rodrigo Garcia <32329949+roG0d@users.noreply.github.com>
2020-01-25 18:35:53 +0100

    Update tests.py

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -53,7 +53,7 @@ def testPDFCSV():
   	"safebrowsing.enabled": True
 	})
 
-	driver = Chrome(executable_path="./chromedriver_ubuntu", 
+	driver = Chrome(executable_path=@"chromedriver_ubuntu", 
 	options=options)
 
 
