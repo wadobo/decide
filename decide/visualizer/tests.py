8cc0fd0d65752d1f72bd7d808bf506d65c544f52
Domin-97 <dominmues@gmail.com>
2020-01-26 16:53:00 +0100

    Merge branch 'master' into domin

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -2,7 +2,9 @@
 # Create your tests here.
 from django.test import TestCase
 from selenium import webdriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 import requests
 import unittest
 import re
@@ -12,17 +14,16 @@ import time
 
 
 
-
 def voting_id_is_positive():
 	'''
-	Descomentar para visualizar al mismo tiempo que se procesa la prueba'''
+	Descomentar para visualizar al mismo tiempo que se procesa la prueba
 	print('Información general de la prueba \n'
-		  + 'Para su correcto funcionamiento debe esta decide corriendo en local o en un servidor por parte de un proveedor de servicios web. \n'
-		  + 'El fin de la prueba es verificar que el id de la votación es un numero mayor que 0')
-	
+		  + 'Para su correcto funcionamiento debe esta decide corriendo en local o en un servidor por parte de un proveedor de servicios web. \n')
+		  + 'El fin de la prueba es verificar que el id de la votación es un numero mayor que 0'
+	'''
 	votingID = input('Qué votación se esta examinando : ')
 
-	driver = webdriver.Chrome(executable_path="./chromedriver_ubuntu")
+	driver = webdriver.Chrome('./chromedriver')
 	driver.get("http://localhost:8000/visualizer/" + votingID)
 	element = driver.find_element_by_id("votingID")
 
@@ -34,50 +35,46 @@ def voting_id_is_positive():
 	verification = float(numbers[0]) > 0
 	print('Criterio para el id superado : ' + str(verification))
 
-def prueba_enviar_email_correcto():
-
-        driver = webdriver.Chrome(executable_path="./chromedriver_ubuntu")
-
-        driver.get("http://127.0.0.1:8000/visualizer/1/")
-
-        driver.find_element_by_xpath('/html/body/div/section/div[2]/div[3]/form/input[2]').send_keys("decide123456789@gmail.com")
-
-        driver.find_element_by_xpath('/html/body/div/section/div[2]/div[3]/form/input[3]').click()
-        
-        verification = str(driver.find_element_by_xpath('/html/body/div/section/div[2]/ul/li/div').text).startswith('El email ha sido enviado!')
-
-        print('Criterio para test_enviar_email_correcto : ' + str(verification)) 
-
-        driver.close()
-
-        driver.quit()
-
-def prueba_enviar_email_mal():
-
-        driver = webdriver.Chrome(executable_path="./chromedriver_ubuntu")
-
-        driver.get("http://127.0.0.1:8000/visualizer/1/")
-
-        driver.find_element_by_xpath('/html/body/div/section/div[2]/div[3]/form/input[2]').send_keys("emailmal.com")
-
-        driver.find_element_by_xpath('/html/body/div/section/div[2]/div[3]/form/input[3]').click()
-        
-        verification = driver.find_elements_by_xpath('/html/body/div/section/div[2]/ul/li/div').count('El email ha sido enviado!') is 0
-
-        driver.close()
-
-        driver.quit()
-
-        
-
-        print('Criterio para test_enviar_email_mal : ' + str(verification)) 
 
 ''' Descomentar para probar las pruebas que aparecen acontinuación
+
 voting_id_is_positive()
-#test_enviar_email_correcto()
-#test_enviar_email_mal()
+
 '''
 
+def pruebaPDFCSV():
+	
+	#Opciones para la descarga
+	options = Options()
+	options.add_experimental_option("prefs", {
+  	"download.default_directory": r"/home/rog0d/Escritorio",
+ 	 "download.prompt_for_download": False,
+  	"download.directory_upgrade": True,
+  	"safebrowsing.enabled": True
+	})
+
+	driver = Chrome(executable_path=".chromedriver_ubuntu", 
+	options=options)
+
+
+	Vid = input('ID de la votación a probar: ')
+	driver.get("http://localhost:8000/visualizer/"+Vid)
+	
+	#busqueda y click de los botones
+	btnPdf = driver.find_element_by_id('pdf').click()
+	btnCsv = driver.find_element_by_id('csv').click()
+	
+	#Contador para las descargas
+	time_to_wait = 10
+	time_counter = 0
+	while not os.path.exists("/home/rog0d/Escritorio"):
+		time.sleep(1)
+		time_counter += 1
+		if time_counter > time_to_wait:break
+	print("descargas realizadas")
+
+
+#testPDFCSV()
 
 
 
